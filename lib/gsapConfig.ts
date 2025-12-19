6f8ce80c044a69d4ab3a295fe2312acf2125ab52
tibogot <tibogotbeatz@gmail.com>
2025-12-19 04:49:53 +0100

    fs

--- lib/gsapConfig.ts
+++ lib/gsapConfig.ts
@@ -3,9 +3,10 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
 import { Draggable } from "gsap/Draggable";
 import { InertiaPlugin } from "gsap/InertiaPlugin";
+import { useGSAP } from "@gsap/react";
 
 // Register all plugins
 gsap.registerPlugin(ScrollTrigger, SplitText, Draggable, InertiaPlugin);
 
-export { gsap, ScrollTrigger, SplitText, Draggable, InertiaPlugin };
+export { gsap, ScrollTrigger, SplitText, Draggable, InertiaPlugin, useGSAP };
 
